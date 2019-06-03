b59fc120174848e5771f96848d3e01564f053395
LiuSongJiang <songjiang.liu@56qq.com>
2019-06-03 16:22:20 +0800

    feat: 增加日志插件

---
+++ src/plugins/hapi-pino.ts
@@ -0,0 +1,29 @@
+import * as Hapi from "hapi";
+import * as HapiPino from "hapi-pino";
+import { IPlugin, IPluginOptions } from "../interfaces/plugin";
+
+async function register(
+  server: Hapi.Server,
+  pluginOptions: IPluginOptions
+): Promise<void> {
+  try {
+    await server.register({
+      plugin: HapiPino,
+      options: {
+        prettyPrint: false,
+        logEvents: ["log", "request", "response", "onPostStart"]
+      }
+    });
+  } catch (error) {
+    console.log(`Error registering hapi-pino plugin: ${error}`);
+  }
+}
+
+export default (): IPlugin => {
+  return {
+    register,
+    info() {
+      return { name: "Hapi plugin for the Pino logger", version: "6.0.1" };
+    }
+  };
+};
