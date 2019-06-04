8e4239878ac0d250c0574618971ccc1bb7657244
LiuSongJiang <songjiang.liu@56qq.com>
2019-06-04 16:06:34 +0800

    chore: 简化打包输出信息，分离webpack文件

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,54 +1,33 @@
 const path = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.config');
 const nodeExternals = require('webpack-node-externals')
 const ReloadServerPlugin = require('reload-server-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: ['./src/app.ts', 'webpack/hot/signal'],
-  // devtool:'cheap-module-source-map',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../dist')
-  },
-  target: "node",
-  resolve: {
-    extensions: [".ts", ".js", ".json"],
-    modules: [path.resolve(__dirname, '../src'), 'node_modules']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              configFile: path.resolve(__dirname, '../tsconfig.json')
-            }
-          }
-        ],
-        exclude: /node_modules/
-      },
-      { test: /\.ts?$/, loader: 'tslint-loader', enforce: 'pre', exclude: /(node_modules)/, }
-    ]
-  },
-  externals: [nodeExternals({
-    whitelist: ['webpack/hot/signal']
-  })],
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new ReloadServerPlugin({
-      script: process.cwd() + "/dist/main.js",
-    }),
-  ],
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000,
-    ignored: /node_modules/
+module.exports = merge(common,
+  {
+    mode: 'development',
+    entry: ['webpack/hot/signal'],
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, '../dist')
+    },
+    externals: [nodeExternals({
+      whitelist: ['webpack/hot/signal']
+    })],
+    stats: "errors-only",
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ReloadServerPlugin({
+        script: process.cwd() + "/dist/main.js",
+      }),
+    ],
+    watch: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: /node_modules/
+    }
   }
-}
+)
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,37 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.config');
 
-module.exports = {
-  mode: 'production',
-  entry: ['./src/app.ts'],
-  // devtool:'cheap-module-source-map',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../bundle')
-  },x
-  target: "node",
-  resolve: {
-    extensions: [".ts", ".js", ".json"],
-    modules: [path.resolve(__dirname, '../src'), 'node_modules']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              configFile: path.resolve(__dirname, '../tsconfig.json')
-            }
-          }
-        ],
-        exclude: /node_modules/
-      }
+module.exports = merge(common,
+  {
+    mode: 'production',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, '../bundle')
+    },
+    plugins: [
+      // new webpack.NoEmitOnErrorsPlugin()
     ]
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
-}
+  }
+)
